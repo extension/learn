54e67db52c3abbc6f3ed44ab887eb4b56a9f0246
Jason Adam Young <jasonadamyoung@gmail.com>
2011-10-20 16:10:21 -0400

    Initial rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanagra::Application.config.secret_token = '00351698094463230fbedee4bea6b05870ee9b99cd1b894a110e67450851923b76fce5d6a9a5bcbc45195dd03b6e248493dc9b9ee517c38767300e0af2287dd1'
