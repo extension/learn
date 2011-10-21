56cfa3bba1f9bd0ea3f072089b07d63c4dd6f1b5
Jason Adam Young <jasonadamyoung@gmail.com>
2011-10-21 14:19:37 -0400

    session model scaffold

---
+++ app/models/session.rb
@@ -0,0 +1,8 @@
+# === COPYRIGHT:
+# Copyright (c) North Carolina State University
+# Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+# see LICENSE file
+
+class Session < ActiveRecord::Base
+end
