9087bfecb26750aa31737d035a319b486e70bc75
Jason Adam Young <jasonadamyoung@gmail.com>
2011-10-21 14:18:53 -0400

    license and some boilerplate mods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
+# === COPYRIGHT:
+# Copyright (c) North Carolina State University
+# Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+# see LICENSE file
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
