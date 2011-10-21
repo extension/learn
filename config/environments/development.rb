9142375cf734406fcb7372609190fb1de86ed777
Jason Adam Young <jasonadamyoung@gmail.com>
2011-10-21 09:36:31 -0400

    Rename Tanagra to Learn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Tanagra::Application.configure do
+Learn::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Tanagra::Application.configure do
+Learn::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
