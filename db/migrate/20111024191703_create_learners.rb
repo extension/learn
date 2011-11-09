4afed750b19ee5e8ccf8fbd68dccefe98f4f8c85
athundle <aaron.hundley@extension.org>
2011-11-09 15:34:29 -0500

    Login Page Added and Devise Fixes
    Added database_authenticatable to learner so we can get our login page routes and so that the redirects will work correctly with devise. Also included is the new login page with the links to login using both twitter and eXtension OpenId.
    
    Also included were some fixes for links and the devise adjustments fixed an issue that was causing an app error when the authentication failed for a service, now the learner is redirected back to the login page if they cannot login to a service. Included in this is a conditional for comments on a event page that displays a link to sign in instead of a post button if the learner is not logged in since we have a authentication requirement for comments.

--- db/migrate/20111024191703_create_learners.rb
+++ db/migrate/20111024191703_create_learners.rb
@@ -1,6 +1,11 @@
 class CreateLearners < ActiveRecord::Migration
   def change
     create_table(:learners) do |t|
+      # creates :email (string) 
+      # creates :encrypted_password field (string) 
+      # we are letting these fields be null to cover certain cases of scientist records like twitter authentication via oauth
+      # that does not give us an email address and we're not storing a pw for it
+      t.database_authenticatable :null => true
       # does not create :remember_token(string), not sure why right now, default is supposed to create it
       # creates :remember_created_at(Datetime)
       t.rememberable
@@ -10,7 +15,7 @@ class CreateLearners < ActiveRecord::Migration
       # creates :current_sign_in_ip (string)
       # creates :last_sign_in_ip (string)
       t.trackable
-      t.string :name, :time_zone, :email, :mobile_number, :null => true
+      t.string :name, :time_zone, :mobile_number, :null => true
       t.boolean :has_profile, :null => false, :default => false
   
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -102,17 +102,18 @@ ActiveRecord::Schema.define(:version => 20111104001730) do
   end
 
   create_table "learners", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password",  :limit => 128
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       :default => 0
+    t.integer  "sign_in_count",                      :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "name"
     t.string   "time_zone"
-    t.string   "email"
     t.string   "mobile_number"
-    t.boolean  "has_profile",         :default => false, :null => false
+    t.boolean  "has_profile",                        :default => false, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
