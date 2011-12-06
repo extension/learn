3769354ee303c75012d3798029ea67876d2fe39c
benmac <ben.macneill@gmail.com>
2011-12-06 16:28:37 -0500

    settings controller

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,17 @@
+# === COPYRIGHT:
+# Copyright (c) North Carolina State University
+# Developed with funding for the National eXtension Initiative.
+# === LICENSE:
+# see LICENSE file
+
+class SettingsController < ApplicationController
+  
+  def profile
+  end
+  
+  def notifications
+  end
+  
+  def portfolio
+  end
+end
