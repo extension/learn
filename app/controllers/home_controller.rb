c5bd2a2f7359aa2c84f1cfe61caeafd11b87d3ff
benmac <ben.macneill@gmail.com>
2011-12-06 12:35:19 -0500

    - wiring up some data for the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,8 @@
 class HomeController < ApplicationController
   
   def index
+    @upcoming_events = Event.find(:all, :conditions => "session_start > '#{Time.now.utc.strftime('%Y-%m-%d %H:%M:%S')}'", :limit => 3, :order => "session_start ASC")
+    @recent_events = Event.find(:all, :conditions => "session_start < '#{Time.now.utc.strftime('%Y-%m-%d %H:%M:%S')}'", :limit => 15, :order => "session_start DESC")
   end
 
 end
