b01f2522f5f4e79de857891742c986f87c038246
Jason Adam Young <jasonadamyoung@gmail.com>
2011-12-13 19:37:28 -0500

    change random question list to be pseudo-random
    Until a fix is found with rangeinput where we can have more than one slider on the same event and they actually work properly when answered - address Issue #72 by always adding a scale, but limiting it to one per event by default.

--- app/models/stock_question.rb
+++ app/models/stock_question.rb
@@ -22,14 +22,23 @@ class StockQuestion < ActiveRecord::Base
   MULTIVOTE_BOOLEAN = 'multivote_boolean'
   
   scope :active, {:conditions => {:active => true}}
+  scope :scale, {:conditions => {:responsetype => SCALE}}
+  scope :notscale, {:conditions => ['responsetype != ?',SCALE]}
+  scope :boolean, {:conditions => {:responsetype => BOOLEAN}}
+  scope :multivote_boolean, {:conditions => {:responsetype => MULTIVOTE_BOOLEAN}}
   
   DEFAULT_RANDOM_COUNT = 3
   
   def self.random_questions(count = DEFAULT_QUESTION_COUNT)
+    list = []
     # we can get away with loading all of them to shuffle
     # because there aren't going to be many stock questions
     # in the database
-    StockQuestion.active.shuffle.slice(0,count)
+    
+    # for now, prevent more than one "random" scale - but will always add a random scale
+    list += StockQuestion.active.scale.shuffle.slice(0,count)
+    list += StockQuestion.active.notscale.shuffle.slice(0,count-1)
+    list
   end
     
 end
