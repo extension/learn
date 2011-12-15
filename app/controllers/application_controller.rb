7cc4a43bfaa1e74987affb4ae508f8fad15338f8
Jason Adam Young <jasonadamyoung@gmail.com>
2011-12-15 12:43:17 -0500

    get rid of fake_learner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,20 +8,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_activity_log_ip
   before_filter :set_time_zone_from_learner
-  
-  
-  def fake_learner
-    if(params[:fake_another_learner])
-      sign_out()
-    end
-    
-    if(!current_learner)
-      if(params[:fake_learner] and learner = Learner.find_by_id(params[:fake_learner]))
-        sign_in(learner)
-      end
-    end
-  end
-  
+
   def set_time_zone_from_learner
     if(current_learner)
       Time.zone = current_learner.time_zone
