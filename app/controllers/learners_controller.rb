e893136e98893d583666dd4f2b9a15237929a0c6
benmac <ben.macneill@gmail.com>
2011-12-18 21:21:46 -0500

    -learning portfolio design -making use of limits added to learner controller

--- app/controllers/learners_controller.rb
+++ app/controllers/learners_controller.rb
@@ -16,10 +16,10 @@ class LearnersController < ApplicationController
       return redirect_to root_url
     end
   
-    @attended_events = @learner.events.attended.order("event_connections.created_at DESC")#.limit(5)
-    @watched_events = @learner.events.watched.order("event_connections.created_at DESC")#.limit(5)
-    @bookmarked_events = @learner.events.bookmarked.order("event_connections.created_at DESC")#.limit(5)
-    @presented_events = @learner.presented_events.order("session_start DESC")#.limit(5)
+    @attended_events = @learner.events.attended.order("event_connections.created_at DESC").limit(5)
+    @watched_events = @learner.events.watched.order("event_connections.created_at DESC").limit(5)
+    @presented_events = @learner.presented_events.order("session_start DESC").limit(5)
+    @bookmarked_events = @learner.events.bookmarked.order("event_connections.created_at DESC").limit(12)
   end
   
   def learning_history  
