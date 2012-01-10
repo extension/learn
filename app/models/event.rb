0402407265cf5ad211d7385db995701c7b55a2e1
Jason Adam Young <jasonadamyoung@gmail.com>
2012-01-10 15:34:50 -0500

    time zone related fixes

--- app/models/event.rb
+++ app/models/event.rb
@@ -154,6 +154,16 @@ class Event < ActiveRecord::Base
     end
   end
   
+  # this is mostly for the mailer situation where
+  # we aren't setting Time.zone for the web request
+  def session_start_for_learner(learner)
+    if(learner.has_time_zone?)
+      self.session_start.in_time_zone(learner.time_zone)
+    else
+      self.session_start.in_time_zone(self.time_zone)
+    end
+  end
+  
   # override timezone writer/reader
   # returns Eastern by default, use convert=false
   # when you need a timezone string that mysql can handle
