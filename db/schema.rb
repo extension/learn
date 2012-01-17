4c650b31dbeba7ae4b8da9e970f51059dcb4e4d3
Jason Adam Young <jasonadamyoung@gmail.com>
2012-01-17 12:53:30 -0500

    versions table schema
    (should have been dealt with with the development merge, but this was easier)

--- db/schema.rb
+++ db/schema.rb
@@ -273,4 +273,15 @@ ActiveRecord::Schema.define(:version => 20120112212724) do
 
   add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
 
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
+
 end
