548d8944712bf223c7cfd7b4e52cb34109e8a770
athundle <aaron.hundley@extension.org>
2012-02-21 10:12:48 -0500

    Use 404 Page for Record Not Found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,7 +58,7 @@ class ApplicationController < ActionController::Base
   end
   
   def record_not_found
-    render :text => "404 Not Found", :status => 404
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
   
 end
