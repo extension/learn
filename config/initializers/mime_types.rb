20fe805e5985baeefd5a8a19a34934e5ca63a638
Jason Adam Young <jasonadamyoung@gmail.com>
2012-04-21 11:32:26 -0400

    Rails 3.2 includes the png mime type
    Removes deprecation warning.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,5 +3,4 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register_alias "image/png", :png
 Mime::Type.register "application/hbs-vcs, text/calendar, text/x-vcalendar", :vcs  
