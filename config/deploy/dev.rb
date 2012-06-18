112e2bb914dad4ee842659ad82863d40e441f751
Jason Adam Young <jasonadamyoung@gmail.com>
2012-06-18 19:13:08 -0400

    dev should use the development branch

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,6 +2,6 @@ set :deploy_to, "/services/apache/vhosts/dev.learn.extension.org/railsroot/"
 if(branch = ENV['BRANCH'])
   set :branch, branch
 else
-  set :branch, 'staging'
+  set :branch, 'development'
 end
 server 'dev.learn.extension.org', :app, :web, :db, :primary => true
