f660b7d5a31c45a7baa593ef8207ee2f64392927
sdnall <s.daniel.nall@gmail.com>
2013-01-09 12:28:27 -0500

    add sitemaps

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'mysql2'
 # xml parsing
 gem 'nokogiri'
 
+gem 'sitemap_generator'
 
 # image upload
 gem 'carrierwave'
---
+++ config/sitemap.rb
@@ -0,0 +1,44 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://#{Settings.urlwriter_host}"
+
+if Settings.sitemap_dir != 'notset'
+  SitemapGenerator::Sitemap.sitemaps_path = Settings.sitemap_dir
+end
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+  
+  Event.find_each do |event|
+    add(event_path(event), :lastmod => event.updated_at)
+  end
+  
+  Conference.find_each do |conference|
+    add(conference_path(conference), :lastmod => conference.updated_at)
+  end
+  
+  Learner.find_each do |learner|
+    add(learner_path(learner), :lastmod => learner.updated_at)
+  end
+  
+end
