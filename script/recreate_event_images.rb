ff4f95dd29df025d07fb857eb8e4c61e9eba32cc
Daniel Nall <s.daniel.nall@gmail.com>
2014-01-09 17:10:35 -0500

    give some feedback when running the script

--- script/recreate_event_images.rb
+++ script/recreate_event_images.rb
@@ -8,8 +8,10 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 
 Event.all.each do |event|
   if event.images.size > 0
+    puts "working on event ##{event.id}"
     event.images.each do |image|
       if File.exists?(Rails.root.join("public#{image.file_url()}"))
+      	puts "  recreating image versions for image ##{image.id}"
         image.file.recreate_versions!
       end
     end
