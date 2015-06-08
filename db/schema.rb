a1339f4f35e9e753df1d6ab460d4b92ae89e2038
Mark Locklear <marklocklear@gmail.com>
2015-06-08 10:06:16 -0400

    fixed merge conflict with master in schema

--- db/schema.rb
+++ db/schema.rb
@@ -180,12 +180,7 @@ ActiveRecord::Schema.define(:version => 20150520204212) do
     t.integer  "raters_count",                           :default => 0,        :null => false
     t.integer  "commentators_count",                     :default => 0,        :null => false
     t.text     "provided_presenter_order"
-<<<<<<< HEAD
-    t.boolean  "requires_registration",                  :default => false,    :null => false
-    t.integer  "evaluator_id"
-=======
     t.boolean  "is_deleted",                             :default => false,    :null => false
->>>>>>> master
   end
 
   add_index "events", ["conference_id"], :name => "conference_ndx"
