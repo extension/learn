060e44f8a9de75b47d3d76ef5c19eb049c36331b
Mark Locklear <marklocklear@gmail.com>
2015-10-08 08:17:26 -0400

    Updated text email language, and added markup scrubber method

--- lib/markup_scrubber.rb
+++ lib/markup_scrubber.rb
@@ -30,4 +30,10 @@ module MarkupScrubber
   def html_to_text(html_string)
     Loofah.fragment(html_string).text
   end
+
+  # adds some cleverness with regard to whitespace and block elements
+  # http://rubydoc.info/github/flavorjones/loofah/master/Loofah/TextBehavior#to_text-instance_method
+  def html_to_pretty_text(html_string)
+    Loofah.fragment(html_string).to_text
+  end
 end
