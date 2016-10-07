ee9e15294dbe7e11c7e97b80765559a703fe8ffc
Jason Adam Young <jayoung@extension.org>
2016-10-07 06:48:23 -0400

    fix honeybadger config

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,6 +61,7 @@ before "deploy:web:enable", "delayed_job:start"
        ln -nfs #{shared_path}/config/sunspot.yml #{release_path}/config/sunspot.yml &&
        ln -nfs #{shared_path}/config/robots.txt #{release_path}/public/robots.txt &&
        ln -nfs #{shared_path}/config/settings.local.yml #{release_path}/config/settings.local.yml &&
+       ln -nfs #{shared_path}/config/honeybadger.yml #{release_path}/config/honeybadger.yml &&
        ln -nfs #{shared_path}/tmpcache    #{release_path}/tmp/cache &&
        ln -nfs #{shared_path}/tmpauth #{release_path}/tmp/auth &&
        ln -nfs #{shared_path}/uploads #{release_path}/public/uploads
