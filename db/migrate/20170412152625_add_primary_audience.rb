cd57bc74b9dddc2f99282f189b2ed34ec37e573c
Jason Adam Young <jayoung@extension.org>
2017-04-12 13:56:21 -0400

    change it everywhere

--- db/migrate/20170412152625_add_primary_audience.rb
+++ db/migrate/20170412152625_add_primary_audience.rb
@@ -1,5 +1,5 @@
 class AddPrimaryAudience < ActiveRecord::Migration
   def change
-    add_column(:events, :primary_audience, :integer, :null => false, :default => Event::AUDIENCE_UNKNOWN)
+    add_column(:events, :primary_audience, :integer, :null => false, :default => Event::AUDIENCE_BLANK)
   end
 end
